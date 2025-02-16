a7e83cda57ead2ac89122915a94471970274d81b
cintiaching <cintiaching@lcjgroup.com>
2025-02-16 16:45:35 +0800

    wrap supervisor in function

--- agents/supervisor.py
+++ agents/supervisor.py
@@ -4,48 +4,25 @@ from agents.coder import create_coder_agent
 from agents.llm import build_llm
 from agents.data_analyst import create_data_analyst_agent
 
-model = build_llm()
 
-# agents
-data_analyst_agent = create_data_analyst_agent()
-coder_agent = create_coder_agent()
+def get_ai_data_scientist():
+    model = build_llm()
 
-# Create supervisor workflow
-workflow = create_supervisor(
-    [data_analyst_agent, coder_agent],
-    model=model,
-    prompt=(
-        "You are a team supervisor managing a data analyst. "
-        "For data analysis task, e.g. inquiry about data or data visualization, use data_analyst_agent. "
-        "For machine learning tasks or general coding task in python, use coder_agent."
+    # agents
+    data_analyst_agent = create_data_analyst_agent()
+    coder_agent = create_coder_agent()
+
+    # Create supervisor workflow
+    workflow = create_supervisor(
+        [data_analyst_agent, coder_agent],
+        model=model,
+        prompt=(
+            "You are a team supervisor managing a data analyst. "
+            "For data analysis task, e.g. inquiry about data or data visualization, use data_analyst_agent. "
+            "For machine learning tasks or general coding task in python, use coder_agent."
+        )
     )
-)
 
-# Compile and run
-app = workflow.compile()
-# example usage of data_analyst_agent
-result = app.invoke({
-    "messages": [
-        {
-            "role": "user",
-            "content": "What are the total sales generated in this fy?"
-        }
-    ]
-})
-print("ANSWER: ")
-print(result["messages"][-1].content)
-
-# example usage of coder_agent
-result = app.invoke({
-    "messages": [
-        {
-            "role": "user",
-            "content": "What will our sales be next quarter? Please use Regression models to predict"
-        }
-    ]
-})
-print("ANSWER: ")
-print(result["messages"][-1].content)
-
-print("full answer")
-print(result["messages"])
+    # Compile and run
+    app = workflow.compile()
+    return app
